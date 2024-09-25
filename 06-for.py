6eb08d51a9100137c05c40ba17a6346d6ae8ce0f
Ronald Espitia <ronald79@gmail.com>
2024-09-24 22:29:23 -0500

    Clase sept 24

--- 06-for.py
+++ 06-for.py
@@ -12,8 +12,8 @@ for letra in palabra:
 
 ''' for con range '''
 
-#for i in range(5):
-    #print(i)
+# for i in range(5):
+# print(i)
 
 ''' Range '''
 # hasta 3 args
@@ -21,8 +21,8 @@ for letra in palabra:
 # stop
 # step
 
-#for j in range(2, 10, 2):
-    #print(j)
+# for j in range(2, 10, 2):
+# print(j)
 
 ''' CONTROL DE FLUJO EN BUCLES '''
 
@@ -33,23 +33,69 @@ while True:
     if respuesta == 'salir':
         break
 
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
 # CONTINUE
 
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
 
+i = 0
+while i < 10:
+    if i == 6:
+        i += 1
+        continue
+    print(i)
+    i += 1
 
 # ELSE
 
+for k in range(5):
+    print(k)
+else:
+    print('El bucle finalizo')
 
 
+''' ENUMERATE '''
+''' sintaxis:
+for indice, elemento in enumerate(iterable) :
+      bloque del for
+'''
+
+frase = 'Buenas noches'
+for index, elem in enumerate(frase):
+    print(f'Indice: {index} - letra:{elem}')
+
+''' ZIP '''
+palabra1 = 'Python5'
+palabra2 = 'VBasic'
+
+for p1, p2 in zip(palabra1, palabra2):
+    print(f'{p1} - {p2}')
+
+for p1, p2 in zip(enumerate(palabra1), enumerate(palabra2)):
+    print(f'elemento 1: {p1[1]} / indice: {p1[0]
+                                           } - elemento 1: {p2[1]} / indice: {p2[0]}')
+
+''' REVERSED '''
+
+for num in reversed(range(7)):
+    print(num)
 
 
+''' SORTED ''' 
 
+[1,2,3]
 
+['1','2','3']
 
+cadena = 'xhtadl'
+cadena2 = '543'
 
-
-
-
-
-
-
+for letra in sorted(cadena2):
+    print(letra)
---
+++ 07-listas.py
@@ -0,0 +1,20 @@
+''' LISTAS '''
+
+# acceso a elementos
+
+lista = [10, 20, 30, 40]
+
+print(lista[-2])
+print(lista[1])
+
+''' Metodos o Funciones de las listas '''
+
+''' append(x) '''
+
+numeros = [101, 205, 602]
+numeros.append(1001)
+print(numeros)
+
+numeros[0] = 202
+
+print(numeros)
